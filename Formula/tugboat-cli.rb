2a139a9f73aad3d97ff5ac7fe258ecc1d9c042d9
Ben Chavet <ben.chavet@lullabot.com>
2020-06-30 23:33:42 -0500

    v2.20.35

--- Formula/tugboat-cli.rb
+++ Formula/tugboat-cli.rb
@@ -2,7 +2,7 @@ class TugboatCli < Formula
   desc "Tugboat CLI"
   homepage "https://tugboat.qa"
   url "https://dashboard.tugboat.qa/cli/macos/tugboat.tar.gz"
-  sha256 "7f9c6ac0330efffb728531f064a4fa4a15269559c66e06ba72f2cb0a29ccac82"
+  sha256 "c62ddd2967be5bc24bc3e66359bdab8402956c1664b99b0627ead354b3a172b5"
   version "2.20.35"
   def install
     bin.install "tugboat"
