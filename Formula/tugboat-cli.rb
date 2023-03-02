cda5780b2d2915a69009731d8c45bca3ef9538ca
Jenkins <jenkins@lullabot.com>
2023-03-01 21:08:49 -0500

    v2.23.5

--- Formula/tugboat-cli.rb
+++ Formula/tugboat-cli.rb
@@ -2,8 +2,8 @@ class TugboatCli < Formula
   desc "Tugboat CLI"
   homepage "https://www.tugboatqa.com"
   url "https://dashboard.tugboatqa.com/cli/macos/tugboat.tar.gz"
-  sha256 "85f1a81ad476fbab893457009729dd20d98b90a96559ed8e295a1d216e31f939"
-  version "2.23.4"
+  sha256 "a38654f7f5ff20c2ab5fd432b0ced6dbe9962da7143a11a0741abdef2d594d34"
+  version "2.23.5"
   def install
     bin.install "tugboat"
   end
