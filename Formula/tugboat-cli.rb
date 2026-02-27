06b3de2dc9061fa88ded2be8019db9352a1e767c
Zequi Vazquez <zequi@lullabot.com>
2026-02-27 16:02:29 +0100

    Set new domain for url property

--- Formula/tugboat-cli.rb
+++ Formula/tugboat-cli.rb
@@ -1,7 +1,7 @@
 class TugboatCli < Formula
   desc "Tugboat CLI"
   homepage "https://www.tugboatqa.com"
-  url "https://dashboard.tugboatqa.com/cli/macos/tugboat.tar.gz"
+  url "https://assets.tugboatqa.com/cli/macos/tugboat.tar.gz"
   sha256 "a1b2ad9c088bcdca51c74116627487a28757cf220ecba556cb4c504c8ca70716"
   version "2.26.9"
   def install
